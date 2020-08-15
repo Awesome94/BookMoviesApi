cf4b01fe2a77441945abcbfa10947fdc2b81a0f5
awesome94 <suubidavid@gmail.com>
2020-08-15 14:28:08 +0300

    Add Cors

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import * as bodyParser from 'body-parser';
-import {Movie} from './db/models/Movie.model';
-import {User} from './db/models/User.model';
+import {Movie} from './db/entity/Movie';
+import {User} from './db/entity/User';
 import { Request, Response } from "express";
 import {hash, genSalt, compare} from 'bcrypt';
 import * as jwt from "jsonwebtoken";
 import {requireJwtMiddleware} from "./middleware/requireJwtMiddleware";
 import dotenv from 'dotenv';
 import {connect} from "./db/db";
+import cors from 'cors';
 
 connect();
 
@@ -22,8 +23,15 @@ app.use(bodyParser.json({
 }));
 
 
-app.get('/', async (req:Request, res:Response)=>{
-    return res.status(200).send({"Welcome": "POST to /api/v1/register to get started"})
+app.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+
+app.post('/', async (req:Request, res:Response)=>{
+    return res.status(200).send({"Welcomed": "POST to /api/v1/register to get started"})
 })
 
 app.post('/api/v1/register', async (req:Request, res:Response)=>{
@@ -46,19 +54,23 @@ app.post('/api/v1/register', async (req:Request, res:Response)=>{
     }
 });
 
-app.post('api/v1/login/:username/:password', async (req:Request, res:Response)=>{
+app.post('/api/v1/login/:username/:password', async (req:Request, res:Response)=>{
     const user = await User.findOne({username: req.params.username})
     if(!user) return res.status(400).send('Username or Password is wrong');
     const validpass = await compare(req.params.password, user.password);
     if(!validpass) return res.status(400).send('Password is wrong');
 
     const token = jwt.sign({_id: user.username}, process.env.TOKEN_SECRET);
-    res.send({"Access Token":token});
+    res.send({"Access Token":token, "username":user.username});
 
 });
 
 
-app.post('api/v1/book', requireJwtMiddleware, async (req:Request, res:Response)=> {
+app.post('/api/v1/book', requireJwtMiddleware, async (req:Request, res:Response)=> {
+    const movieBooked = await Movie.findOne({name: req.body.name})
+
+    if(movieBooked) return res.status(400).send('Movie already booked');
+
     const movie  = new Movie();
     movie.name = req.body.name;
     movie.plot_summary = req.body.summary;
@@ -69,7 +81,7 @@ app.post('api/v1/book', requireJwtMiddleware, async (req:Request, res:Response)=
 });
 
 
-app.get('api/v1/booked/:username', async(req, res)=>{
+app.get('api/v1/booked/:indentifier', async(req, res)=>{
     const movies = await Movie.find();
     res.send(movies);
 });
